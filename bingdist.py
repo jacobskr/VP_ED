0101a3a05ec733ba52d523a3d37c62dff5d51b94
jacobskr <31374947+jacobskr@users.noreply.github.com>
2018-09-22 10:48:51 -0400

    Bing distance/time iteration
    Iterate over origins and destinations using Bing Map API to get distance in miles and travel time in minutes. Saves as CSV

--- bingdist.py
+++ bingdist.py
@@ -18,17 +18,22 @@ tripdf = pd.read_excel(r"C:\Users\User\Desktop\Test Files\Addresses.xlsx")
 # Create dictionary
 traveldict = {"Origin": [], "Destination": [], "Distance": [], "Duration": []}
 
+# Iterate over each origin/destination and append into traveldict
 i = 0
 
-for t in tripdf:
+for row in tripdf.itertuples(index=True, name='Pandas'):
+    # Bing Maps API address input encoding
     encodedOrig = urllib.parse.quote(tripdf.Origin[i], safe='')
     encodedDest = urllib.parse.quote(tripdf.Destination[i], safe='')
     
+    # Bing Maps API URL
     routeUrl = "http://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=" + encodedOrig + "&wp.1=" + encodedDest + "&key=" + bingMapsKey
     
+    # Bing Maps URL request and response
     request = urllib.request.Request(routeUrl)
     response = urllib.request.urlopen(request)
     
+    # JSON and formatting
     r = response.read().decode(encoding="utf-8")
     result = json.loads(r)
     
@@ -47,4 +52,12 @@ for t in tripdf:
     traveldict["Distance"].append(dist)
     traveldict["Duration"].append(dur)
     
+    print(i)
     i += 1
+
+travelpd = pd.DataFrame(traveldict)
+
+writer = pd.ExcelWriter(r'C:\Users\User\Desktop\Test Files\travelpd.xlsx')
+travelpd.to_excel(writer,'Sheet1', index = False)
+writer.save()
+
